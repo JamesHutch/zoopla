9d71c65dc14c7b379fce8a74c827dbe1f46a36a2
James Hutchinson <j@meshut.ch>
2014-02-28 17:29:45 +0000

    Add Apache license

--- api.go
+++ api.go
@@ -1,3 +1,8 @@
+// Copyright 2014 James Hutchinson. All rights reserved.
+//
+// Use of this source code is governed by the Apache v2
+// license that can be found in the LICENSE file.
+
 package zoopla
 
 const baseURL = "http://api.zoopla.co.uk/api/v1/"
